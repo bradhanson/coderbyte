42b3723d2a2f5d2abe31001a17f38c2477a082be
Bradley Hanson <brad@bradhanson.com>
2018-06-25 13:45:35 -0400

    Added medium/prime_time

---
+++ medium/prime_time.js
@@ -0,0 +1,18 @@
+/**
+ * Have the function primeTime(num) take the num parameter being passed and
+ * return the string true if the parameter is a prime number, otherwise return
+ * the string false. The range will be between 1 and 2^16.
+ * @param  {number} num
+ * @return {boolean}
+ */
+function primeTime(num) {
+    for (let i = 2, max = Math.floor(num / 2); i < max; i++) {
+        if (num % i === 0) {
+            // divisible!  not prime
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = primeTime;
