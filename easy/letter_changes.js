087ae4a03dfaa965c46210c96fb9061c84dce6f4
Bradley Hanson <brad@bradhanson.com>
2018-06-25 12:46:33 -0400

    Factored out caesarCipher()

--- easy/letter_changes.js
+++ easy/letter_changes.js
@@ -8,26 +8,79 @@
  * @return {string}
  */
 function letterChanges(str) {
-    let validCharacters =
-        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    let newString = '';
-
-    for (let i = 0; i < str.length; i++) {
-        if (validCharacters.includes(str[i])) {
-            let char = str.charCodeAt(i);
-            newString +=
-                str[i] === 'z' || str[i] === 'Z'
-                    ? String.fromCharCode(char - 25)
-                    : String.fromCharCode(char + 1);
-        } else {
-            newString += str[i];
-        }
-    }
-
+    let newString;
+    newString = caesarCipher(str, 1);
     newString = capitalizeVowels(newString);
 
     return newString;
+}
+
+/*
+ * Helper functions below
+ */
+
+/**
+ * Returns a new string with a Caesar cipher applied
+ * https://en.wikipedia.org/wiki/Caesar_cipher
+ * @param  {string} str
+ * @param  {number} key
+ * @return {string}
+ */
+function caesarCipher(str, key) {
+    const newString = str
+        .split('')
+        .map(char => (isAlpha(char) ? shiftAlphaChar(char, key) : char))
+        .join('');
+
+    /**
+     * Shifts a character by a delta value
+     * @param  {string} char  Assumed to be an alphabetic character
+     * @param  {number} delta
+     * @return {string}
+     */
+    function shiftAlphaChar(char, delta) {
+        const charCode = char.charCodeAt(0);
+
+        const alphabetStart = isUpperCase(char)
+            ? 'A'.charCodeAt(0)
+            : 'a'.charCodeAt(0);
+
+        const distanceFromA = charCode - alphabetStart;
+        const newDistanceFromA = (distanceFromA + delta) % 26;
+        const newCharCode = newDistanceFromA + alphabetStart;
+
+        return String.fromCharCode(newCharCode);
+    }
+
+    return newString;
+}
+
+/**
+ * Checks if a string only includes alphabetic characters
+ * @param  {string}  str
+ * @return {Boolean}
+ */
+function isAlpha(str) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return str.split('').every(char => alphabet.includes(char));
+}
+
+/**
+ * Checks if a string does not contain any lowercase characters
+ * @param  {string}  str
+ * @return {Boolean}
+ */
+function isUpperCase(str) {
+    return str === str.toUpperCase();
+}
+
+/**
+ * Checks if a string does not contain any uppercase characters
+ * @param  {string}  str
+ * @return {Boolean}
+ */
+function isLowerCase(str) {
+    return str === str.toLowerCase();
 }
 
 /**
@@ -36,11 +89,11 @@ function letterChanges(str) {
  * @return {string}
  */
 function capitalizeVowels(str) {
-    const VOWELS = 'aeiou';
+    const vowels = 'aeiou';
 
     const newString = str
         .split('')
-        .map(char => (VOWELS.includes(char) ? char.toUpperCase() : char))
+        .map(char => (vowels.includes(char) ? char.toUpperCase() : char))
         .join('');
 
     return newString;
