2ce68be56de1b05f5f8308548e13a25b94fd3feb
Bradley Hanson <brad@bradhanson.com>
2018-07-01 17:52:55 -0400

    Fixed return type to spec and fixed comparison error

--- medium/prime_time.js
+++ medium/prime_time.js
@@ -3,16 +3,16 @@
  * return the string true if the parameter is a prime number, otherwise return
  * the string false. The range will be between 1 and 2^16.
  * @param  {number} num
- * @return {boolean}
+ * @return {string}
  */
 function primeTime(num) {
-    for (let i = 2, max = Math.floor(num / 2); i < max; i++) {
+    for (let i = 2, max = Math.floor(num / 2); i <= max; i++) {
         if (num % i === 0) {
             // divisible!  not prime
-            return false;
+            return 'false';
         }
     }
-    return true;
+    return 'true';
 }
 
 module.exports = primeTime;
