ea54965cef2fbe4ca6d6a29733a7efca56479b69
Bradley Hanson <brad@bradhanson.com>
2018-07-01 17:55:41 -0400

    Changed tests to reflect return type in spec

--- medium/prime_time.test.js
+++ medium/prime_time.test.js
@@ -2,13 +2,13 @@ const primeTime = require('./prime_time');
 
 describe('primeTime()', () => {
     test('correctly checks if number is prime', () => {
-        expect(primeTime(2)).toBe(true);
-        expect(primeTime(3)).toBe(true);
-        expect(primeTime(5)).toBe(true);
-        expect(primeTime(8171)).toBe(true);
+        expect(primeTime(2)).toBe('true');
+        expect(primeTime(3)).toBe('true');
+        expect(primeTime(5)).toBe('true');
+        expect(primeTime(8171)).toBe('true');
     });
 
     test('correctly handles input of 1', () => {
-        expect(primeTime(1)).toBe(true);
+        expect(primeTime(1)).toBe('true');
     });
 });
